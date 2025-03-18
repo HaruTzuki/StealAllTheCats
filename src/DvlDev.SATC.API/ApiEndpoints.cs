0e4b3402d0f75da2942f08a685a7f6ae0864b734
Dimtiris Vlachopoulos <dvlachopoulos@dvldev.com>
2025-03-18 19:44:41 +0200

    Added Api Endpoints Constants

--- src/DvlDev.SATC.API/ApiEndpoints.cs
+++ src/DvlDev.SATC.API/ApiEndpoints.cs
@@ -1,6 +1,15 @@
 ﻿namespace DvlDev.SATC.API;
 
-public class ApiEndpoints
+public static class ApiEndpoints
 {
-	
+	private const string ApiBase = "api";
+
+	public static class Cats
+	{
+		private const string Base = $"{ApiBase}/cats";
+
+		public const string Create = Base;
+		public const string Get = $"{Base}/{{idOrCatId}}";
+		public const string GetAll = Base;
+	}
 }
